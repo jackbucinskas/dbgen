24c0f719a51cd27684c1f1bd0a2f37eb3d141116
Jack Bucinskas <jack.bucinskas@gmail.com>
2020-04-29 17:37:12 +1000

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for (x = 0; x < 10; x++) {
     // var ID = chance.integer({ min: 1000, max: 9999 });
     var FNAME = chance.first();
     var LNAME = chance.last();
-    var DOB = chance.birthday({string: true});
+    var DOB = chance.birthday({ string: true });
     var BALANCE = chance.floating({ min: 0, max: 10000 });
 
     userValues.push("INSERT INTO USERS VALUES(" + x + ", '" + FNAME + "', '" + LNAME + "', '" + DOB + "', " + BALANCE + ");" );
